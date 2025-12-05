5b4bd35ebba79035f50611bc7de6b1fafe1b4cf9
Nikhilsethi7668 <nikhilsethin494@gmail.com>
2025-12-05 14:37:01 +0530

    docker

--- frontend/admin/src/services/socket.js
+++ frontend/admin/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5001'; // Backend URL
+const URL = 'http://localhost:5000'; // Backend URL
 
 export const socket = io(URL, {
     autoConnect: false
