04a1251a2b9aff0d3cdd1ae91d2f03cc44b88092
Nikhilsethi7668 <nikhilsethin494@gmail.com>
2025-12-05 14:42:24 +0530

    update

--- frontend/admin/src/services/socket.js
+++ frontend/admin/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:5000'; // Backend URL
+const URL = 'http://localhost:7000'; // Backend URL
 
 export const socket = io(URL, {
     autoConnect: false
