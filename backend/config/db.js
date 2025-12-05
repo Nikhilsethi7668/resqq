40c6615801deb79b6327520354387f32a93d0203
Nikhilsethi7668 <nikhilsethin494@gmail.com>
2025-12-05 14:20:21 +0530

    dp

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const config = require('./production');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(config.MONGO_URI);
+        const conn = await mongoose.connect("mongodb+srv://resq:resq1234@cluster0.vrl9lv4.mongodb.net/?appName=Cluster0");
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
