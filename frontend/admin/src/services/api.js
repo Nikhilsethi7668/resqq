2df28736e9e19bf2b5eb7006b0a7ed5fbee2ccdf
Nikhilsethi7668 <nikhilsethin494@gmail.com>
2025-12-05 11:54:07 +0530

    apis connectionupdate

--- frontend/admin/src/services/api.js
+++ frontend/admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api'
+    baseURL: 'http://localhost:7003/api'
 });
 
 // Add token to requests automatically
