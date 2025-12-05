ded26983fe7fe07c792b0ac904ee19c55a92c688
Nikhilsethi7668 <nikhilsethin494@gmail.com>
2025-12-05 13:53:03 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "http://localhost:5177"
+  "http://localhost:5177",
+  "http://localhost:7002",
+  "http://localhost:7001",
 ];
 
 const io = new Server(server, {
